ae7c0d237fa271f04704227b1861c35624b23006
Romano Lucchesi <romano@lucchesi.co.uk>
2024-01-11 09:26:49 +0000

    fix: buttonz

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,13 +10,24 @@
         font-weight: 200;
         text-align: center;
     }
+
+    a {
+        text-decoration: underline;
+        color: white;
+    }
 </style>
 
 <div class="app bg-black text-white h-100 align-items-center h-100">
     <div class={"align-items-center h-100 row mx-0"}>
         <slot/>
         <footer class="pb-3">
-            <small>Made on Svelte with {"<3"} © Romano Lucchesi 2021-{year}</small>
+            <small>
+                Made on Svelte with {"<3"}
+                • <a href="https://status.romanolucchesi.com">Service Status</a>
+                • <a href="https://access.romanolucchesi.com">Access</a>
+                • <a href="https://email.atomicvr.co.uk">Email</a>
+                • © Romano Lucchesi 2021-{year}
+            </small>
             <br/>
             <img alt={"favicon"} src={"favicon.ico"}/>
         </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,6 @@
     }
 </style>
 
-
 <div>
     <h1>Romano Lucchesi</h1>
     <img alt={"me"} class={"my-3 my-face"} height={300} src={"me.jpg"}/>
@@ -28,28 +27,29 @@
         programmer from the UK, focused on devops and full stack development
     </p>
 
-    <Button href={"https://www.youtube.com/channel/UCnrxILtJAgXZeNFUqPrmd7A"} variant={"danger"}>
-        <i class="bi bi-youtube"/>
-    </Button>
-    <Button href={"https://www.reddit.com/user/trunkyredstone"} variant={"reddit"}>
-        <i class="bi bi-reddit"/>
-    </Button>
-    <Button href={"https://stackoverflow.com/users/14927628/trunky"} variant={"orange"}>
-        <i class="bi bi-stack-overflow"/>
-    </Button>
-    <Button href={"/portfolio"} variant={"warning"}>
-        Portfolio
-    </Button>
-    <Button href={"https://open.spotify.com/user/trunkyredstone?si=8680b6f0953d4cb5"} variant={"success"}>
-        <i class="bi bi-spotify"/>
-    </Button>
-    <Button href={"https://twitter.com/imtrunky"} variant={"info"}>
-        <i class="bi bi-twitter"/>
-    </Button>
-    <Button href={"https://twitch.tv/imtrunky"} variant={"purple"}>
-        <i class="bi bi-twitch"/>
-    </Button>
-    <br/> <br/>
+    <div class="d-flex justify-content-center w-100 mb-4">
+        <Button href={"https://www.youtube.com/channel/UCnrxILtJAgXZeNFUqPrmd7A"} variant={"danger"}>
+            <i class="bi bi-youtube"/>
+        </Button>
+        <Button href={"https://www.reddit.com/user/trunkyredstone"} variant={"reddit"}>
+            <i class="bi bi-reddit"/>
+        </Button>
+        <Button href={"https://stackoverflow.com/users/14927628/trunky"} variant={"orange"}>
+            <i class="bi bi-stack-overflow"/>
+        </Button>
+        <Button href={"/portfolio"} variant={"warning"}>
+            Portfolio
+        </Button>
+        <Button href={"https://open.spotify.com/user/trunkyredstone?si=8680b6f0953d4cb5"} variant={"success"}>
+            <i class="bi bi-spotify"/>
+        </Button>
+        <Button href={"https://twitter.com/imtrunky"} variant={"info"}>
+            <i class="bi bi-twitter"/>
+        </Button>
+        <Button href={"https://twitch.tv/imtrunky"} variant={"purple"}>
+            <i class="bi bi-twitch"/>
+        </Button>
+    </div>
     <Button href={"mailto:me@romanolucchesi.com"} variant={"info"}>
         Email me
     </Button>
